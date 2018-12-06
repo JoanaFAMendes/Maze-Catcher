0f71f3c8d0f83e1b1c428e1c84c74b7a0a30f833
JoanaFAMendes <janafam@hotmail.com>
2018-12-06 20:41:00 +0000

    v1

---
+++ Assets/Scripts/MainMenuS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenuS : MonoBehaviour {
+
+    public TextMeshProUGUI highscoreText;
+
+    void Start()
+    {
+        highscoreText.text = "Highest Score: " + ((int)PlayerPrefs.GetFloat("Highscore")).ToString();
+    }
+    public void PlayGame()
+    {
+       // Debug.Log("UUU");
+        SceneManager.LoadScene("Level 01");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void BackMenu()
+    {
+        /*GetComponent<Score>().ResetScore();
+        GetComponent<Trigger>().ResetPlayer();*/
+        SceneManager.LoadScene("Begining");
+    }
+
+}
